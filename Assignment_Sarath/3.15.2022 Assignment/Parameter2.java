7ef768b236085931615aa61457e222210271e702
sarath u <ukm7773@gmail.com>
2022-03-15 20:54:43 +0530

    Assignment for 14th and 15th march

---
+++ Assignment_Sarath/3.15.2022 Assignment/Parameter2.java
@@ -0,0 +1,20 @@
+package marchfifteen;
+//with parameter and return type
+public class Parameter2 {
+	public static void main(String[] args) {
+		 int a = 4;
+	        int b = 5;
+	        
+	 
+	        // Calling the function with 2 parameters
+	        int result=calc(a, b);
+	        System.out.println(result);
+	}
+	        public static Integer calc(int x, int y)
+	        {
+	            int sum = x + y;
+	           return sum;
+	        }
+	
+
+}
