32b57adec937b20a8d558aef3b0dac8c9a8b7b46
BhargavSai-17 <bhargavsaivasa@gmail.com>
2022-03-15 19:58:26 +0530

    Object Class Example

---
+++ ObjectClass/Class.java
@@ -0,0 +1,12 @@
+package ObjectClass;
+
+class Class{
+    private String m;
+    Class(){
+        this.m="Objectclass";
+    }
+    @Override
+    public String toString() {
+        return m;
+    }
+}
