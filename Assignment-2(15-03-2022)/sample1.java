cec3a10459c20e2c79ee69a34cdfc12a6767364c
nandhini1219 <nandhini.t1219@gmail.com>
2022-03-15 22:10:19 +0530

    15-03-2022

--- Assignment-2(15-03-2022)/sample1.java
+++
@@ -1,9 +0,0 @@
-public class sample1 {
-        public static void main(String[] args) {
-            // TODO Auto-generated method stub
-            Class obj=new Class();
-            System.out.println(obj);
-        }
-    }
-
-
