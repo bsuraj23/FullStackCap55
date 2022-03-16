5c3186c0b0386d59a63de3f60c8b572bd6664850
nandhini1219 <nandhini.t1219@gmail.com>
2022-03-16 21:05:19 +0530

    Assignment

---
+++ march16th/Main.java
@@ -0,0 +1,15 @@
+
+
+import java.util.ArrayList;
+
+ class Main {
+  public static void main(String[] args) { 
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("nandhini");
+    name.add("Nan");
+    name.add("nandh");
+    name.add("Nandhu");
+    name.remove(0);
+    System.out.println(name);
+  } 
+}
