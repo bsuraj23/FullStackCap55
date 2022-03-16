3024dd133f818b3da65545131fbd2d51aee70c42
TeenahBlessy <teenahdpaul0703@gmail.com>
2022-03-16 13:37:34 +0530

    Assignment for 14th and 15th march

---
+++ Assignments for 13th and 14th march _Teenah/Abs/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mds.abstraction"}];updateSearchResults();
---
+++ Assignments for 13th and 14th march _Teenah/Abs/src/Dog.java
@@ -0,0 +1,7 @@
+
+public class Dog extends West {
+		  public void animalSound() {
+		    
+		    System.out.println("Output 1");
+		  }
+		}
