2a8914fa05bc808a4533ed91a1ee92c1e08598c8
vijayram9 <vijayram2000.ch@gmail.com>
2022-03-16 23:06:18 +0530

    March 16

---
+++ Assignments 16-03-2022/Code/test.java
@@ -0,0 +1,14 @@
+
+
+import java.util.ArrayList;
+
+class test {
+  public static void main(String[] args) {
+    ArrayList<String>  names = new ArrayList<String>();
+    names.add("Nandhini");
+    names.add("nandhu");
+    names.add("nan");
+    names.add("nandh");
+    System.out.println(names);
+  }
+}
