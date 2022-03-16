e774d15e63634a67fb856b85138cb9ad3907d197
Kirakp <kiranphadimani@gmail.com>
2022-03-16 22:00:52 +0530

    16th march assignment

---
+++ 16th March/arrayListDelete.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class arrayListDelete {
+	public static void main(String[] args) { 
+	    ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    cars.remove(0);
+	    System.out.println(cars);
+	  } 
+
+}
