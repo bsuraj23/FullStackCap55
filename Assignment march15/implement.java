b9ae9fcd8b2bf86af1c55f7f6f9992ac61d353cc
nandhini1219 <nandhini.t1219@gmail.com>
2022-03-17 11:47:25 +0530

    updated march15 assignments

---
+++ Assignment march15/implement.java
@@ -0,0 +1,7 @@
+public class implement {
+    public static void main(String [] args){
+        sample s1 = new sample();
+        sample s2 = new sample(17,15);
+        sample s3 = new sample(s1);
+    }
+}
