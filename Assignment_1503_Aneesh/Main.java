9ed7b79ae90d95c1bd2aa563180ee215065a79a5
Aneesh0631 <101163524+Aneesh0631@users.noreply.github.com>
2022-03-17 12:23:53 +0530

    15/03-Assignment

---
+++ Assignment_1503_Aneesh/Main.java
@@ -0,0 +1,45 @@
+package com.company.aneesh;
+import java.util.*;
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+// Java program to illustrate If statement
+
+                Sample2333  SampleObj23 = new Sample2333();
+                //  System.out.println(SampleObj23.add()  );
+                // System.out.println(SampleObj23.add(1,2)  );
+                System.out.println(SampleObj23.add("Ramana SAI")  );
+            }
+        }
+
+        class Sample2333
+        {
+            int a=90;
+            byte b= 89;
+
+            String name = "Rachana";
+            //non praprmeter
+            int add(){
+                //comment
+                String name = "sdfkgsdf";
+                int c= 23+23;
+                System.out.println("I am a non paramtereted and with rtetuurn value function");
+                return c ;
+            }
+            int add(int a, int b){
+                //comment
+                int c= a+b;
+                System.out.println("I am parametered  nd with rtetuurn value function");
+                return c;
+            }
+            boolean add(String str){
+                //comment
+                System.out.println("Hellow " +str  +"can you hear me ?");
+                return false;
+            }
+    }
+
