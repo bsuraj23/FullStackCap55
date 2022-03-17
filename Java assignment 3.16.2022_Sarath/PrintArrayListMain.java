d4d303589e8cb6d0ae2bebebfb82ff2a0fb591ef
sarath u <ukm7773@gmail.com>
2022-03-17 11:51:56 +0530

    Updated Assignments 1

--- Java assignment 3.16.2022_Sarath/PrintArrayListMain.java
+++ Java assignment 3.16.2022_Sarath/PrintArrayListMain.java
@@ -9,10 +9,20 @@ public class PrintArrayListMain {
         arl.add("Kia");
         arl.add("Tesla");
         arl.add("MG");
+
+	System.out.println("Array list before removing");
  
         System.out.println("Elements of ArrayList are:");
         for (int i = 0; i < arl.size(); i++) {
             System.out.println(arl.get(i) + " ");
         }
+System.out.println("Elements of ArrayList after removing :");
+
+        for (int i = 0; i < arl.size(); i++) {
+            System.out.println(arl.get(i) + " ");
+        }
+
+
+	
     }
 }
