94130729757e6c808334c85ac5556bf9196e0a07
ardra04 <ardras124@gmail.com>
2022-03-17 12:21:57 +0530

    I am adding Assignment Folder

---
+++ Ardra_Assignments/14 March Assignment/14 March Coding Questions/Classesandobjects.java
@@ -0,0 +1,10 @@
+public class Classesandobjects {  //class
+        int a = 10;
+
+        public static void main(String[] args) {
+                Classesandobjects myObj1 = new Classesandobjects();  // Object 1
+                Classesandobjects myObj2 = new Classesandobjects();  // Object 2
+                System.out.println(myObj1.a);
+                System.out.println(myObj2.a);
+        }
+}
