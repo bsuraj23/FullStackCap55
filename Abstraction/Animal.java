6389f1938492325d8d20170c5bc2922cd598041d
BhargavSai-17 <bhargavsaivasa@gmail.com>
2022-03-17 11:04:59 +0530

    Abstractio Examples

---
+++ Abstraction/Animal.java
@@ -0,0 +1,8 @@
+package com.Abstraction;
+
+abstract class Animal {
+    public abstract void animalSound();
+    public void sleep() {
+        System.out.println("Sleep");
+    }
+}
---
+++ Abstraction/Main.java
@@ -0,0 +1,9 @@
+package com.Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog md = new Dog(); // Create a Pig object
+        md.animalSound();
+        md.sleep();
+    }
+}
