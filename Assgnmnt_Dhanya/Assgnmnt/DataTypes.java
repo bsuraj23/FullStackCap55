f69f672b537781a9b39bf22dc7bf1fd418332b4f
Dhanyajr <dhanyakrishnan5gold.com>
2022-03-17 13:38:07 +0530

    Adding remaining assignment progrms with modifications

---
+++ Assgnmnt_Dhanya/Assgnmnt/DataTypes.java
@@ -0,0 +1,42 @@
+
+public class DataTypes {
+	public static void main(String[] args) {
+    	
+	    boolean flag = true;
+	    System.out.println(flag);    // prints true
+	    
+	    byte range;
+	    range = 124;
+	    System.out.println(range);    // prints 124
+	  
+	    short temperature;
+	    temperature = -200;
+	    System.out.println(temperature);  // prints -200
+	  
+	    int range1 = -4250000;
+	    System.out.println(range1);  // print -4250000
+	 
+	    long range2 = -42332200000L;
+	    System.out.println(range2);    // prints -42332200000
+	  
+	    double number = -42.3;
+	    System.out.println(number);  // prints -42.3
+	  
+	    float number1 = -42.3f;
+	    System.out.println(number1);  // prints -42.3
+	 
+	    char letter = '\u0051';
+	    System.out.println(letter);  // prints Q
+	  
+	    char letter1 = '9';
+	    System.out.println(letter1);  // prints 9
+	    	
+	    char letter2 = 65;
+	    System.out.println(letter2);  // prints A
+
+	    String s = "Java Programming";
+	    System.out.println(s);  // prints A
+
+	  }
+
+}
