e01872b29a902f84225cdcc288757256d4aecfda
kaluvaideepakreddy <kaluvaideepakreddy@gmail.com>
2022-03-21 10:14:19 +0530

    Assignment

---
+++ march16/AddArrayList/Main.java
@@ -0,0 +1,16 @@
+package Com.AddArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Benz");
+        cars.add("Maruthi");
+        cars.add("Tata");
+        System.out.println(cars);
+    }
+}
+
