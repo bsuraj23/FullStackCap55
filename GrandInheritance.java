96aad8f19eac9410525814b429fdc706b9f8ebd7
Govind N <gokugovindgoku@gmail.com>
2022-03-22 16:23:52 +0530

    codes of 22/03/22

---
+++ GrandInheritance.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+class Grandparent {
+	public void Print()
+	{
+		System.out.println("Grandparent's Print()");
+	}
+}
+
+class Parent extends Grandparent {
+	public void Print()
+	{
+		System.out.println("Parent's Print()");
+	}
+}
+
+class Child extends Parent {
+	public void Print()
+	{
+		super.Print();
+		System.out.println("Child's Print()");
+	}
+}
+
+public class GrandInheritance {
+	public static void main(String[] args)
+	{
+		Child c = new Child();
+		c.Print();
+	}
+}
