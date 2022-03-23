859b331d85509a72775b1c85b146c42c3f4bd489
nandhini1219 <nandhini.t1219@gmail.com>
2022-03-23 22:32:14 +0530

    Assignment of 22 and 23

--- march15th/Main .java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String [] args){
-        sample s1 = new sample();
-        sample s2 = new sample(17,15);
-        sample s3 = new sample(s1);
-    }
-}
