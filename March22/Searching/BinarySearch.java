651e1d9874b322c6f20bf4b1e223bd4ff9304ddd
kaluvaideepakreddy <kaluvaideepakreddy@gmail.com>
2022-03-23 22:39:16 +0530

    Assignment

---
+++ March22/Searching/BinarySearch.java
@@ -0,0 +1,29 @@
+package Com.Searching;
+
+class BinarySearch {
+    int binarySearch(int arr[], int l, int r, int x)
+    {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 20, 30, 40, 50, 60 };
+        int n = arr.length;
+        int x = 30;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index "
+                    + result);
+    }
+}
