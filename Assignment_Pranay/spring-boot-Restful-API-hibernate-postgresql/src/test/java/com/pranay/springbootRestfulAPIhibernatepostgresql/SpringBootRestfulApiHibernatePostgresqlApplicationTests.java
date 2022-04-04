271f2ceea2e3bad742f169846c50139ca1d84b23
PranayRoberts <86553739+PranayRoberts@users.noreply.github.com>
2022-04-04 09:40:08 +0530

    Added spring assignment

---
+++ Assignment_Pranay/spring-boot-Restful-API-hibernate-postgresql/src/test/java/com/pranay/springbootRestfulAPIhibernatepostgresql/SpringBootRestfulApiHibernatePostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.springbootRestfulAPIhibernatepostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulApiHibernatePostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
