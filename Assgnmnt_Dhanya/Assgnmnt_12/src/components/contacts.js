295fa6abe40504bd2d566b49b0badcb817692b74
Dhanyajr <dhanyakrishnan5gold.com>
2022-04-12 23:43:20 +0530

    Adding new assignment

---
+++ Assgnmnt_Dhanya/Assgnmnt_12/src/components/contacts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+    
+const Contacts = ({ contacts }) => {
+  return (
+    <div>
+         <center><p>Hello this is a paragraph.</p></center>
+         
+     </div>
+  )
+};
+
+export default Contacts
